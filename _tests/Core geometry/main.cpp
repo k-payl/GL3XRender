672b02a2ab40ecb35cb110726cd05836a88b3e7a
Konstantin Payl <k.payl@plamee.local>
2016-07-25 19:22:06 +0300

    run profiler by dafault in 'CoreGeometry'

--- _tests/Core geometry/main.cpp
+++ _tests/Core geometry/main.cpp
@@ -60,6 +60,7 @@ void DGLE_API Render(void *pParameter)
 		MatrixIdentity() // zero point for all transformations
 	);
 
+
 	pMesh1->Draw();
 }
 
@@ -91,6 +92,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	{
 		if (SUCCEEDED(pEngineCore->InitializeEngine(NULL, APP_CAPTION, TEngineWindow(SCREEN_WIDTH, SCREEN_HEIGHT, false, false, MM_4X, EWF_ALLOW_SIZEING), 33u, static_cast<E_ENGINE_INIT_FLAGS>(EIF_LOAD_ALL_PLUGINS | EIF_NO_SPLASH))))
 		{
+			pEngineCore->ConsoleExecute("rnd2d_profiler 2");
 			pEngineCore->AddProcedure(EPT_INIT, &Init);
 			pEngineCore->AddProcedure(EPT_RENDER, &Render);
 			pEngineCore->AddProcedure(EPT_UPDATE, &Update);
